0c7408ea4c7730692e7a98f5120e4b5c2aae8d6d
孙承秀 <>
2020-04-08 17:42:23 +0800

    初始化工程

---
+++ SCXPromiseKit/SCXPromiseKit/SCXPromiseKit.h
@@ -0,0 +1,19 @@
+//
+//  SCXPromiseKit.h
+//  SCXPromiseKit
+//
+//  Created by 孙承秀 on 2020/4/8.
+//  Copyright © 2020 RongCloud. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SCXPromiseKit.
+FOUNDATION_EXPORT double SCXPromiseKitVersionNumber;
+
+//! Project version string for SCXPromiseKit.
+FOUNDATION_EXPORT const unsigned char SCXPromiseKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SCXPromiseKit/PublicHeader.h>
+
+
