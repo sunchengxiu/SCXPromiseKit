e7e9a3157ea78dff4c96a711a6c495ea241079f1
孙承秀 <>
2020-05-20 16:35:27 +0800

    race test

--- SCXPromiseKit/SCXPromiseKitTests/SCXPromiseRaceTest.m
+++ SCXPromiseKit/SCXPromiseKitTests/SCXPromiseRaceTest.m
@@ -7,12 +7,21 @@
 //
 
 #import <XCTest/XCTest.h>
-
+#import "SCXPromise+Recover.h"
+#import "SCXPromise+Catch.h"
+#import "SCXPromise+Async.h"
+#import "SCXPromisePrivate.h"
+#import "SCXTestHelper.h"
+#import "SCXPromise+ThenAsync.h"
+#import "SCXPromise+ThenSync.h"
+#import "SCXPromise+Validate.h"
+#import "SCXPromise+Race.h"
 @interface SCXPromiseRaceTest : XCTestCase
 
 @end
 
 @implementation SCXPromiseRaceTest
+NSErrorDomain const SCXPromiseRaceTestErrorDomain = @"com.rongcloud.promise.raceTestError";
 
 - (void)setUp {
     // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -21,7 +30,44 @@
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
 }
+- (void)test{
+    SCXPromise *p1 = [SCXPromise async:^(SCXPromiseFulfillBlock  _Nonnull fulfill, SCXPromiseRejectBlock  _Nonnull reject) {
+        SCXDelay(0.1, ^{
+            fulfill(@(1));
+        });
+    }];
+    SCXPromise *p2 = [SCXPromise async:^(SCXPromiseFulfillBlock  _Nonnull fulfill, SCXPromiseRejectBlock  _Nonnull reject) {
+        SCXDelay(2, ^{
+            reject([NSError errorWithDomain:SCXPromiseRaceTestErrorDomain code:42 userInfo:nil]);
+        });
+    }];
+    SCXPromise *p = [[[SCXPromise race:@[p2,p1]] then:^id _Nullable(id  _Nullable value) {
+        XCTAssertEqualObjects(value, @1);
 
+    }] catch:^(NSError * _Nonnull error) {
+        XCTFail();
+    }] ;
+    SCXWaitForPromisesWithTimeout(15);
+}
+- (void)test1{
+    SCXPromise *p1 = [SCXPromise async:^(SCXPromiseFulfillBlock  _Nonnull fulfill, SCXPromiseRejectBlock  _Nonnull reject) {
+        SCXDelay(1, ^{
+            fulfill(@(1));
+        });
+    }];
+    SCXPromise *p2 = [SCXPromise async:^(SCXPromiseFulfillBlock  _Nonnull fulfill, SCXPromiseRejectBlock  _Nonnull reject) {
+        SCXDelay(0.1, ^{
+            reject([NSError errorWithDomain:SCXPromiseRaceTestErrorDomain code:42 userInfo:nil]);
+        });
+    }];
+    SCXPromise *p = [[[SCXPromise race:@[p2,p1]] then:^id _Nullable(id  _Nullable value) {
+        XCTFail();
+        return value;;
+    }] catch:^(NSError * _Nonnull error) {
+        XCTAssertEqual(error.code, 42);
+    }] ;
+    SCXWaitForPromisesWithTimeout(15);
+}
 - (void)testExample {
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
